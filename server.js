2ac0f1bedbb588a2348d24b283f0621ab078e11e
Bebeau <kyle@theinitgroup.com>
2018-10-05 13:37:38 -0700

    change server ports

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,10 +29,10 @@ app.use((req, res, next) => {
 });
 
 // Serve any static files
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // console.log that your server is up and running
